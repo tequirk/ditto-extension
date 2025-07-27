49015779270690e67ce8025f10ded13b2a2cb5c6
nate <nate.benson@mealmagic.com>
2025-07-26 22:10:14 -0400

    Label padding change

--- src/components/ui/FormField.vue
+++ src/components/ui/FormField.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label class="block text-xs text-[#888] dark:text-[#bbb] mb-1 cursor-grab">
+    <label class="block text-xs pl-1.5 text-[#888] dark:text-[#bbb] mb-1 cursor-grab">
       {{ label }}
     </label>
     <input
